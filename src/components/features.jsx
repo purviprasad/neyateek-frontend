30d8c259c11664cca5bbd5b033fddc026319e9ea
sayuj-fmo <sayuj@findmyoptions.com>
2024-01-25 21:52:43 +0530

    add Product Logos and Links

--- src/components/features.jsx
+++ src/components/features.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import RaffleraLogo from "../assets/rafflera-logo.png";
+import PFTLogo from "../assets/pft-logo.png";
+import './features.css'
 export const Features = (props) => {
   const handleClick = (e) => {
 
@@ -16,7 +18,8 @@ export const Features = (props) => {
               <div key={`${d.title}-${i}`} className={"col-xs-12 col-md-6"}>
                 <a href={d.title === 'Rafflera' ? 'https://rafflera.web.app/' : 'https://www.playfuturetraders.com/'} target='_blank' style={{ color: '#777' }}>
                   {" "}
-                  <i className={d.icon}></i>
+                  {/* <i className={d.icon}></i> */}
+                  <img className='feature-img' src={d.title === 'Rafflera' ? RaffleraLogo : PFTLogo} alt="" />
                   <h3>{d.title}</h3>
                   <p>{d.text}</p>
                   {i === 2 && <br />}
